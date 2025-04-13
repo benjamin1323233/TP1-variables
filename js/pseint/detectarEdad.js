ddfbac9eb7b8de17c5c10e015d776ec36c09d4bd
Quiros Benjamin <bq170921@gmail.com>
2025-04-13 18:15:40 -0300

    realice el ejercicio de detectar edad de pseint

---
+++ js/pseint/detectarEdad.js
@@ -0,0 +1,6 @@
+//pide la edad y decir si es o no mayor de edad
+const edad = prompt("ingresa tu edad")
+if (edad >= 18){
+    document.writeln("eres mayor de edad")
+}else{
+    document.writeln("no eres mayor de edad")}
