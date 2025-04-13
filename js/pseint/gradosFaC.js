1ca6886c2dbc941f1c6fd6b73228d9e45f470541
Quiros Benjamin <bq170921@gmail.com>
2025-04-13 18:55:04 -0300

    realice el ejercicio de grados f a celcius

---
+++ js/pseint/gradosFaC.js
@@ -0,0 +1,4 @@
+ //convertir grados fahrenheit a celsius
+ const fahrenheit = parseInt(prompt("ingresa los grados fahrenheit"))
+ const final = (5/9)*(fahrenheit-32)
+ document.writeln("la temperatura es "+ final)
