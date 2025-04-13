eedd60d46c1437e93e6bdaaba813a66183ebecb4
Quiros Benjamin <bq170921@gmail.com>
2025-04-13 19:13:02 -0300

    reLICE EL EJERCICIO DE ANCHO Y LARGO

---
+++ js/pseint/perimetroyarea.js
@@ -0,0 +1,5 @@
+// calcular el perimetro y el area de un rectangulo
+const ancho = parseInt(prompt("ingresa el ancho"));
+const largo = parseInt(prompt("ingresa el largo"));
+document.writeln("el perimetro es de " + 2 * (largo + ancho));
+document.writeln("y el area es de ", largo * ancho)
