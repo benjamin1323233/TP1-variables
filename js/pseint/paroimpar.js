8a72e7142147af0b9a8458beeeb7d206916d10fc
Quiros Benjamin <bq170921@gmail.com>
2025-04-13 19:03:59 -0300

    realice el ejercicio de pseint de saber si es par o impar

---
+++ js/pseint/paroimpar.js
@@ -0,0 +1,7 @@
+//detectar si el numero es par o  impar
+const numero = parseInt(prompt("escribe un numero para saber si es par o impar"))
+if (numero % 2 == 0){
+    document.writeln("tu numero es par")
+}else{
+    document.writeln("tu numero es impar")
+}
