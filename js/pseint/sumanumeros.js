ab1b215b7376f7122d0154b3081a4317e19ad3f0
Quiros Benjamin <bq170921@gmail.com>
2025-04-16 16:25:12 -0300

    realice el ejercicio de suma de 2 numeros

---
+++ js/pseint/sumanumeros.js
@@ -0,0 +1,5 @@
+//realizar una suma de dos numeros:Crear un algoritmo que permita ingresar al usuario 2 numeros
+const primerNumero = parseInt(prompt("ingrese el primer numero"))
+const segundoNumero = parseInt(prompt("ingresa el segundo numero"))
+const resultado = primerNumero + segundoNumero
+document.writeln("la suma de los numero es de ", resultado)
